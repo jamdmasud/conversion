903fd66aeda01d6e3b2888c33906cd04407e712e
Masud <masud@nybsys.com.bd>
2017-12-21 10:27:22 +0600

    Push notification & rabbit mq

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Suprema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program : UnitTest
+    {
+        private AccessControl ac = new AccessControl();
+
+        protected override void runImpl(UInt32 deviceID)
+        {
+            ac.execute(sdkContext, deviceID, true);
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Title = "Test for access control";
+            program.run();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PushNotification/PushNotification/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushNotification
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string conStr = ConfigurationManager.ConnectionStrings["sqlConString"].ConnectionString;
+            Console.WriteLine("Starting dependency...");
+            SqlDependency.Start(conStr);
+            Console.WriteLine("Dependency started." + Environment.NewLine);
+            RegisterNotification();
+            
+            Console.WriteLine("Waiting for change notification......");
+            Console.ReadLine();
+
+            SqlDependency.Stop(conStr);
+        }
+
+        public static void RegisterNotification()
+        {
+            string conStr = ConfigurationManager.ConnectionStrings["sqlConString"].ConnectionString;
+            string sqlCommand = @"SELECT [ContactID],[ContactName],[ContactNo] from [dbo].[Contacts]";
+             using (SqlConnection con = new SqlConnection(conStr))
+            {
+                SqlCommand cmd = new SqlCommand(sqlCommand, con); 
+                if (con.State != System.Data.ConnectionState.Open)
+                {
+                    con.Open();
+                }
+                cmd.Notification = null;
+                SqlDependency sqlDep = new SqlDependency(cmd);
+                sqlDep.OnChange += sqlDep_OnChange; 
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    // nothing need to add here now
+                }
+            }
+        }
+
+
+        static void sqlDep_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Type == SqlNotificationType.Change)
+            {
+                SqlDependency sqlDep = sender as SqlDependency;
+                sqlDep.OnChange -= sqlDep_OnChange;
+                Console.WriteLine("Something changed in database...."); 
+                RegisterNotification();
+            }
+        }
+    }
+
+}
